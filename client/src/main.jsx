ab090b91ea0778efe1eb826db9b6cda49335a9d8
Abdei Mustafa Muhammad Mubtasim <mubtasimf443@gmail.com>
2024-07-15 11:46:22 +0600

    Create main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,18 @@
+/* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  ﷺ  InshaAllah */
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import './css/main.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    
+  </React.StrictMode>,
+)
